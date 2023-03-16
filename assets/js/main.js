e092166bb7d627491e1b46d29b57452119eb6e77
Antonio <antoniogallegos086@gmail.com>
2023-03-16 11:59:38 -0600

    Buscador JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
 console.log('Proyecto SCRUM');
+document.addEventListener("keyup", e=>{
+
+    if (e.target.matches("#buscador")){
+  
+        if (e.key ==="Escape")e.target.value = ""
+  
+        document.querySelectorAll(".badge rounded-pill bg-col1 text-body-tertiary shadow-sm ms-articulo").forEach(letra =>{
+  
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?letra.classList.remove("filtro")
+              :letra.classList.add("filtro")
+        })
+  
+    }
+  
+  
+  })
