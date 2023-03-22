b4182c86db196b2c9cf4836f06c101fda3a2ea4a
Moisés <olivares.moy@gmail.com>
2023-03-22 10:16:16 -0600

    Limita nombre de tarjeta y busqueda de tags

--- README.md
+++ README.md
@@ -1,14 +1,4 @@
-go# Proyecto de Ingeniería de Software
-# git init
-# git add .
-# git add <Archivo>
-# git status
-# git diff
-# git commit -m "Version1"
-# git log
-# git branch nombreRama
-# git checkout nombreRama
-# git push -u origin nombreRama
-# git remote add origin [url]
-# git remote -v
-# git clone [url]
+# Id's y class names
+#newTag
+#Tags
+.tagTarjeta
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,17 @@
-console.log('Proyecto SCRUM');
+// Función para limitar a 2 líneas el nombre de la tarjeta
+document.querySelectorAll(".-b-expander").forEach(function(el) {
+    el.addEventListener("click", () => {
+      el.classList.toggle("-b-text-undexpanded");
+    });
+});
+// Función para realizar la busqueda de tags
+document.addEventListener("keyup", e=>{
+    if (e.target.matches("#buscador")){
+        if (e.key ==="Escape")e.target.value = ""
+        document.querySelectorAll(".articulo").forEach(fruta =>{
+            fruta.textContent.toLowerCase().includes(e.target.value.toLowerCase())
+              ?fruta.classList.remove("filtro")
+              :fruta.classList.add("filtro")
+        })
+    }
+});
